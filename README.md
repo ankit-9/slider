d39b8e45872ff8528a7cb1dade9137ee93c4d9f7
dws-ankit <ankit@rankwatch.com>
2016-02-19 13:48:23 +0530

    add direction change option

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # slider
 slider for slide image and text
 
---open this folder in your localhost
-  http://localhost/slider-master
-
 --need internet connection to load bootstrap and jquery CDN files
 
--- remaining task :
+-- start slider animation by calling
+    )$(".simple-slider").startSlider();
 
-    ) break main function in subfunction
-    ) add on hover stop slider
-    ) add next prv. button
-    ) add slider indicators
-    ) add slide/text direction change option (left or right)
-    ) add width change option (small,medium and large) or responsive width
+-- you can change image direction left/right
+    )data-img-direction="left"
+
+-- you can change text direction left/right
+    )data-text-direction="right"
+
+-- you can change slide interval
+    )data-slidechange-interval="3000"
+
+-- you can change animation interval
+    )data-animation-interval="1500"
+
+-- upcoming feature
+    ) on hover stop slider
+    ) next prv. button
+    ) slider indicators
+    ) width change option (small,medium and large) or responsive width
 
--- js/slider.js
+++ js/slider.js
@@ -7,55 +7,46 @@ jQuery.fn.extend({
       var lastIndex = (imgSelecter.length - 1);
       var slideInterval = $(that).data('slidechange-interval');
       var animationInterval = $(that).data('animation-interval');
+      var imgDirection = $(that).data('img-direction');
+      var textDirection = $(that).data('text-direction');
+      var imgPropStart = (imgDirection=="left")?{left:400}:{right:400};
+      var imgPropEnd = (imgDirection=="left")?{left:0}:{right:0};
+      var textPropStart = (textDirection=="left")?{left:400}:{right:400};
+      var textPropEnd = (textDirection=="left")?{left:0}:{right:0};
+
+      imgSelecter.addClass((imgDirection=="left")?"left":"right");
+      textSelecter.addClass((textDirection=="left")?"left":"right");
 
       loop = setInterval(function () {
         currentSlide = currentIndex;
         nextSlide = currentIndex + 1;
         currentIndex++;
-
         // call animation function
-        that.slideAnimateLeft(imgSelecter,currentSlide,nextSlide,animationInterval);
-        that.slideAnimateRight(textSelecter,currentSlide,nextSlide,animationInterval);
-
-         if(currentIndex >= lastIndex){ // check index.length
-          clearInterval(loop); // break setInterval
-          // reset slide img and text index
+        that.slideAnimate(imgSelecter,currentSlide,nextSlide,imgPropStart,imgPropEnd,animationInterval);
+        that.slideAnimate(textSelecter,currentSlide,nextSlide,textPropStart,textPropEnd,animationInterval);
+        if(currentIndex >= lastIndex){ // check index.length
+          clearInterval(loop); // break setInterval and reset slide img and text index
           currentIndex = 0;
           imgSelecter.eq(currentIndex).addClass("next");
           textSelecter.eq(currentIndex).addClass("next");
           setTimeout(function(){
-            that.slideAnimateLeft(imgSelecter,nextSlide,currentIndex,animationInterval);
-            that.slideAnimateRight(textSelecter,nextSlide,currentIndex,animationInterval);
+            that.slideAnimate(imgSelecter,nextSlide,currentIndex,imgPropStart,imgPropEnd,animationInterval);
+            that.slideAnimate(textSelecter,nextSlide,currentIndex,textPropStart,textPropEnd,animationInterval);
             setTimeout(function(){that.startSlider();},animationInterval);
           },slideInterval); // end setTimeout
         } // end if
       }, slideInterval); // end setInterval
     },
-    slideAnimateLeft : function(select,current,next,interval){
+    slideAnimate : function(select,current,next,start,end,interval){
          // change class
         select.removeClass("active");
-        select.eq(current).addClass("active");
+        select.eq(current).addClass("active move");
         select.eq(next).addClass("next");
-
         // animation
-        select.eq(current).animate({left:400},interval,function(){
-            select.eq(current).removeClass("active").removeAttr("style");
+        select.eq(current).animate(start,interval,function(){
+            select.eq(current).removeClass("active move").removeAttr("style");
         });
-        select.eq(next).animate({left:0},interval,function(){
-          select.eq(next).removeClass("next").addClass("active").removeAttr("style");
-        });
-    },
-    slideAnimateRight : function(select,current,next,interval){
-         // change class
-        select.removeClass("active");
-        select.eq(current).addClass("active");
-        select.eq(next).addClass("next");
-
-        // animation
-        select.eq(current).animate({right:400},interval,function(){
-            select.eq(current).removeClass("active").removeAttr("style");
-        });
-        select.eq(next).animate({right:0},interval,function(){
+        select.eq(next).animate(end,interval,function(){
           select.eq(next).removeClass("next").addClass("active").removeAttr("style");
         });
     }
